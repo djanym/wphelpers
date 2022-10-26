8298f0723adc19b8db74d3ff59166e235d33ce03
Nael Concescu <djanym@users.noreply.github.com>
2022-10-26 13:53:28 +0300

    adds category tree method

--- packages/FrontHelper.php
+++ packages/FrontHelper.php
@@ -16,4 +16,40 @@ class FrontHelper {
 		);
 	}
 
+	public static function get_category_tree( $args = '' ) {
+		$defaults = array(
+			'taxonomy' => 'category',
+		);
+
+		$args = wp_parse_args( $args, $defaults );
+
+		return self::get_term_tree( $args );
+	}
+
+	public static function get_term_tree( $args = '' ) {
+		$defaults = array(
+			'hide_empty'   => true,
+			'echo'         => false,
+			'hierarchical' => 1,
+			'show_count'   => false,
+			'parent'       => 0,
+			'orderby'      => 'name',
+			'order'        => 'ASC',
+			'tree'         => [],
+		);
+
+		$args = wp_parse_args( $args, $defaults );
+
+		$cats = get_terms( $args );
+
+		if ( $cats ) {
+			foreach ( $cats as &$cat ) {
+				$args['parent'] = $cat->term_id;
+				$cat->childs  = self::get_term_tree( $args );
+			}
+		}
+
+		return $cats;
+	}
+
 }
