89e8570f803bdcaf07050ab3b29b6abd78080160
Nael Concescu <djanym@users.noreply.github.com>
2024-03-28 08:36:29 +0300

    adds has_child_pages()

--- packages/FrontHelper.php
+++ packages/FrontHelper.php
@@ -3,53 +3,75 @@
 namespace Ricubai\WPHelpers;
 
 class FrontHelper {
-	public static function body_class( $class ) : void {
-		add_filter(
-			'body_class',
-			static function( $classes ) use ( $class ) {
-				$classes[] = $class;
+    public static function body_class( $class ) : void {
+        add_filter(
+            'body_class',
+            static function( $classes ) use ( $class ) {
+                $classes[] = $class;
 
-				return $classes;
-			},
-			10,
-			2
-		);
-	}
+                return $classes;
+            },
+            10,
+            2
+        );
+    }
 
-	public static function get_category_tree( $args = '' ) {
-		$defaults = array(
-			'taxonomy' => 'category',
-		);
+    public static function get_category_tree( $args = '' ) {
+        $defaults = array(
+            'taxonomy' => 'category',
+        );
 
-		$args = wp_parse_args( $args, $defaults );
+        $args = wp_parse_args( $args, $defaults );
 
-		return self::get_term_tree( $args );
-	}
+        return self::get_term_tree( $args );
+    }
 
-	public static function get_term_tree( $args = '' ) {
-		$defaults = array(
-			'hide_empty'   => true,
-			'echo'         => false,
-			'hierarchical' => 1,
-			'show_count'   => false,
-			'parent'       => 0,
-			'orderby'      => 'name',
-			'order'        => 'ASC',
-			'tree'         => [],
-		);
+    public static function get_term_tree( $args = '' ) {
+        $defaults = array(
+            'hide_empty'   => true,
+            'echo'         => false,
+            'hierarchical' => 1,
+            'show_count'   => false,
+            'parent'       => 0,
+            'orderby'      => 'name',
+            'order'        => 'ASC',
+            'tree'         => [],
+        );
 
-		$args = wp_parse_args( $args, $defaults );
+        $args = wp_parse_args( $args, $defaults );
 
-		$cats = get_terms( $args );
+        $cats = get_terms( $args );
 
-		if ( $cats ) {
-			foreach ( $cats as &$cat ) {
-				$args['parent'] = $cat->term_id;
-				$cat->childs  = self::get_term_tree( $args );
-			}
-		}
+        if ( $cats ) {
+            foreach ( $cats as &$cat ) {
+                $args['parent'] = $cat->term_id;
+                $cat->childs    = self::get_term_tree( $args );
+            }
+        }
 
-		return $cats;
-	}
+        return $cats;
+    }
+
+    /**
+     * Check if a page has child pages.
+     *
+     * @param int $post_id The post ID.
+     *
+     * @return bool
+     */
+    public static function has_child_pages( $post_id ) {
+        // Query child pages.
+        $child_pages = get_pages( array(
+            'child_of'    => $post_id,
+            'post_type'   => 'page',
+            'post_status' => 'publish',
+        ) );
+
+        if ( $child_pages ) {
+            return true;
+        }
+
+        return false;
+    }
 
 }
--- packages/functions.php
+++ packages/functions.php
@@ -1,75 +1,85 @@
 <?php
 
 use Ricubai\WPHelpers\ContentHelper;
+use Ricubai\WPHelpers\FrontHelper;
 
 if ( ! function_exists( 'echo_if' ) ) :
-	/**
-	 * Helper for printing some string if a variable equals to a value.
-	 *
-	 * @param mixed  $var   Variable to compare.
-	 * @param mixed  $value Value to compare with variable.
-	 * @param string $print Output string in case variable equals passed value.
-	 */
-	function echo_if( $var, $value, $print ) {
-		echo $var === $value ? wp_kses_post( $print ) : '';
-	}
+    /**
+     * Helper for printing some string if a variable equals to a value.
+     *
+     * @param mixed  $var   Variable to compare.
+     * @param mixed  $value Value to compare with variable.
+     * @param string $print Output string in case variable equals passed value.
+     */
+    function echo_if( $var, $value, $print ) {
+        echo $var === $value ? wp_kses_post( $print ) : '';
+    }
 endif;
 
 if ( ! function_exists( 'echo_if_else' ) ) :
-	/**
-	 * Helper for printing some string if a variable equals to a value, otherwise print else.
-	 *
-	 * @param mixed $var        Variable to compare.
-	 * @param mixed $value      Value to compare with variable.
-	 * @param mixed $print_if   Outputs string in case variable equals passed value.
-	 * @param mixed $print_else Outputs string in case variable does not equals passed value.
-	 */
-	function echo_if_else( $var, $value, $print_if, $print_else ) {
-		echo $var === $value ? wp_kses_post( $print_if ) : wp_kses_post( $print_else );
-	}
+    /**
+     * Helper for printing some string if a variable equals to a value, otherwise print else.
+     *
+     * @param mixed $var        Variable to compare.
+     * @param mixed $value      Value to compare with variable.
+     * @param mixed $print_if   Outputs string in case variable equals passed value.
+     * @param mixed $print_else Outputs string in case variable does not equals passed value.
+     */
+    function echo_if_else( $var, $value, $print_if, $print_else ) {
+        echo $var === $value ? wp_kses_post( $print_if ) : wp_kses_post( $print_else );
+    }
 endif;
 
 if ( ! function_exists( 'get_cf_content' ) ) :
-	/**
-	 * Helper for getting custom field content. Works for text, textarea, wysiwyg, etc. fields.
-	 * Will not work for fields which can return `false` value.
-	 * Checks if current page has value, then checks global content (option) value.
-	 *
-	 * @param string $cf_name Variable to compare.
-	 */
-	function get_cf_content( $cf_name ) {
-		$content = get_field( $cf_name, get_the_ID() );
-		if ( $content ) {
-			return $content;
-		}
-		$content = get_field( $cf_name, 'option' );
-		if ( $content ) {
-			return $content;
-		}
+    /**
+     * Helper for getting custom field content. Works for text, textarea, wysiwyg, etc. fields.
+     * Will not work for fields which can return `false` value.
+     * Checks if current page has value, then checks global content (option) value.
+     *
+     * @param string $cf_name Variable to compare.
+     */
+    function get_cf_content( $cf_name ) {
+        $content = get_field( $cf_name, get_the_ID() );
+        if ( $content ) {
+            return $content;
+        }
+        $content = get_field( $cf_name, 'option' );
+        if ( $content ) {
+            return $content;
+        }
 
-		return null;
-	}
+        return null;
+    }
 endif;
 
 if ( ! function_exists( 'get_cover_image_src' ) ) :
-	/**
-	 * Helper for getting post cover image URL. Can be a WP featured image or from a custom field.
-	 *
-	 * @return string|false Taxonomy slug or false.
-	 */
-	function get_cover_image_src() {
-		return ContentHelper::get_cover_image_src() ?? false;
-	}
+    /**
+     * Helper for getting post cover image URL. Can be a WP featured image or from a custom field.
+     *
+     * @return string|false Taxonomy slug or false.
+     */
+    function get_cover_image_src() {
+        return ContentHelper::get_cover_image_src() ?? false;
+    }
 endif;
 
 if ( ! function_exists( 'the_cover_image' ) ) :
-	/**
-	 * Helper for getting post cover image HTML tag. Can be a WP featured image or from a custom field.
-	 *
-	 * @return void Taxonomy slug or false.
-	 */
-	function the_cover_image() : void {
-		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-		echo ContentHelper::get_cover_image();
-	}
+    /**
+     * Helper for getting post cover image HTML tag. Can be a WP featured image or from a custom field.
+     *
+     * @return void Taxonomy slug or false.
+     */
+    function the_cover_image() : void {
+        // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+        echo ContentHelper::get_cover_image();
+    }
+endif;
+
+if ( ! function_exists( 'has_child_pages' ) ) :
+    /**
+     * Check if a page has child pages.
+     */
+    function has_child_pages( $post_id ) : bool {
+        return FrontHelper::has_child_pages( (int) $post_id );
+    }
 endif;
