229785e9130148721ceeaa1d94b531d964e745b3
Nael <concescu@nael.pro>
2024-04-23 14:55:44 +0300

    adds disable_editor_fullscreen

--- packages/WpBlocksHelper.php
+++ packages/WpBlocksHelper.php
@@ -114,6 +114,8 @@ class WpBlocksHelper {
                     wp.domReady( () => {
                         wp.blocks.unregisterBlockStyle( 'core/image', 'default' );
                         wp.blocks.unregisterBlockStyle( 'core/image', 'rounded' );
+                        wp.blocks.unregisterBlockStyle( 'core/button', 'fill' );
+                        wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
                     });
                 }
                 JS;
